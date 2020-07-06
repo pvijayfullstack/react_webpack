d113ec8a747cf6c8a6d5dfcdde5451fde6d72204
vijay <pvijay127@gmail.com>
2020-07-06 10:00:15 +0530

    added charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
-import HelloWorld from './components/hello-world';
+import Main from './components/main';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   render() {
-    return <HelloWorld title="Hello from React webpack" />;
+    return <Main />;
   }
 }
 
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import DisplayOne from './DisplayOne'
+import DisplayTwo from './DisplayTwo'
 
-const Main = ({title}) => (
-  <div>{title}</div>
+import style from './main.scss'
+
+const Main = () => (
+  <div className={style["main"]}>
+    <div className={style["container"]}>
+     <div className={style["navigation"]}>
+      <ul>
+        <li className={style["Operations"]}>Operations</li>
+        <li className={style["Executive"]}>Executive</li>
+        <li className={style["Product-Knowledge"]}>Product Knowledge</li>
+      </ul>
+      <div className={style["content"]}>
+        <div className={style["Rectangle-519"]}>
+          <span className={style["Brewlogix-OG-Tavern-Location"]}>Brewlogix OG Tavern-Location</span>
+        </div>
+        <br/><br/>
+        <div className={style['content-body']}>
+          <DisplayOne style={style}/>
+        </div>
+
+        <div className={style['content-body2']}>
+          <DisplayTwo style={style}/>
+        </div>
+      </div>
+      <br/><br/>
+     </div>
+  </div>
+   </div>
 );
-
-Main.propTypes = {
-  title: PropTypes.string,
-};
 
 export default Main;
